42c43f44550c17c9e9ad3a021ee3e9ebe378de00
Mb-stephane3 <stephanembopda033@gmail.com>
2025-01-28 23:04:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Nigeria"
 
-global.ownernumber = '2348165846414'  //creator number
-global.ownername = '𝑻𝒐𝒙𝒙𝒊𝒄 𝑻𝒆𝒄𝒉' //owner name
+global.ownernumber = '237679478850'  //creator number
+global.ownername = 'pain Akats' //owner name
 global.botname = '𝒁𝒆𝒏𝒐𝒏 𝒁𝒐𝒈𝒓𝒂𝒕𝒊𝒔' //name of the bot
 
 // MENU TYPE
